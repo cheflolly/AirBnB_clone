1d701ecc492e3017032e17b47e04167c91c1c960
Abdul-Razak <Muhammadjamiuabdulrazak@gmail.com>
2023-03-08 17:21:26 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python3
-''' BaseModel module.. form the baseclass for other project models '''
+""" Module that contain the basemodel for the project"""
 from uuid import uuid4
 from models import storage
 from datetime import datetime
 
 
 class BaseModel:
-    ''' Define all common attributes/methods '''
+    """A base model class to create an instance"""
 
     def __init__(self, *args, **kwargs):
-        ''' Deserialize a serialized class or initialize a new one '''
-        if not kwargs:
+        """Initialize the Basemdodel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(kwargs[key])
-                if key != '__class__':
-                    setattr(self, key, value)
-    
+            storage.save()
+
     def __str__(self):
-        ''' Override default implementation of __str__ method '''
-        fmt = '[{}] ({}) {}'
-        return (fmt.format(type(self).__name__, self.id, self.__dict__))
+        """ Return string representation of an object"""
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        ''' Update `self.updated_at` with the current datetime '''
+        """Save changes made to an object"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        ''' Return a new dictionary with key and strformatted datetime '''
-        dict_ = {}
+        """Convert object to dictionary"""
+        new_dict = {}
+        new_dict["__class__"] = type(self).__name__
+
         for key, value in self.__dict__.items():
-            if key == 'created_at' or key == 'updated_at':
-                dict_[key] = value.isoformat()
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = value.isoformat()
             else:
-                if not value:
-                    pass
-                else:
-                    dict_[key] = value
-        dict_['__class__'] = type(self).__name__
-        return (dict_)
+                new_dict[key] = value
+
+        return new_dict
